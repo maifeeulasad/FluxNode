9b72cd7f37f8629bc867cc87a449b17af9043265
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-09-25 18:51:40 +0600

    [config]: basic vite configuration for dev;

---
+++ vite.example.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  root: 'example',
+  build: {
+    outDir: '../example-dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      'flux-node': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+});
